8463b9d91caade5469be4c066be4ec7aaa10d6d4
evpoluyanov <evpoluyanov@gmail.com>
2026-08-03 09:32:00 +0000

    Sprint 22: prepare production foundation

--- app/database.py
+++ app/database.py
@@ -1,14 +1,23 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
-DATABASE_URL = "sqlite:///./decision_matrix.db"
-
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./decision_matrix.db",
 )
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={
+            "check_same_thread": False,
+        },
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 
 SessionLocal = sessionmaker(
